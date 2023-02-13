dbcc2763992f898a2c7605edf49a19167c9d5ec0
MORRIS <morris24kariuki@gmail.com>
2023-02-13 04:22:52 +0300

    maded changes to the Product component

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
+import useStyles from './styles'
 
 function Product({product}) {
+
+    const classes = useStyles()
+
   return (
     <Card className={classes.root} >
       <CardMedia className={classes.media} image='' title={product.name} />
@@ -18,7 +22,7 @@ function Product({product}) {
         <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+        <IconButton aria-label="Add to Cart">
           <AddShoppingCart />
         </IconButton>
       </CardActions>
