003f15300b74f048e05cd108cccf42382a8b4c09
MORRIS <morris24kariuki@gmail.com>
2023-02-13 04:51:10 +0300

    added styling to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/commerce.png'
+import useStyles from './styles';
 
 function Navbar() {
+    const classes = useStyles();
+
   return (
     <>
     <AppBar position="fixed" className={classes.appBar} color="inherit">
       <Toolbar>
-        <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+        <Typography  to="/" variant="h6" className={classes.title} color="inherit">
           <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> MyStore
         </Typography>
         <div className={classes.grow} />
         {location.pathname === '/' && (
         <div className={classes.button}>
-          <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+          <IconButton  to="/cart" aria-label="Show cart items" color="inherit">
             <Badge badgeContent={totalItems} color="secondary">
               <ShoppingCart />
             </Badge>
