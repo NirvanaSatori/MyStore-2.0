f814186f6e5a29cc801888f4a6fef876c097f0e8
MORRIS <morris24kariuki@gmail.com>
2023-02-17 20:17:57 +0300

    added Routes to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce'
 import { Navbar, Products } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [products, setProducts] = useState([])
@@ -29,10 +30,23 @@ function App() {
   // console.log(products)
 
   return (
-    <div>
-        <Navbar totalItems={cart.total_items} />
-        <Products products={products} onAddToCart={handleAddToCart}/>
+    <Router>
+    <div style={{ display: 'flex' }}>
+      <CssBaseline />
+      <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+      <Switch>
+        <Route exact path="/">
+          <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+        </Route>
+        <Route exact path="/cart">
+          <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+        </Route>
+        <Route path="/checkout" exact>
+          <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   )
 }
 
